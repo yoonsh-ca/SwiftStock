4e6e9969e85faad670682addcb68fff59c9545fe
yoonsh-ca <yoonsh330@gmail.com>
2025-08-27 11:57:52 -0600

    feat: Complete add new item to inventory, and read sync

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,11 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, database } from '../api/firebase';
 
 export default function Inventory() {
   const [items, setItems] = useState([]);
   const [userId, setUserId] = useState(null);
+  const [isAdding, setIsAdding] = useState(false);
+
+  const [productName, setProductName] = useState('');
+  const [cost, setCost] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [category, setCategory] = useState('');
+
+  const handleAddItem = () => {
+    setIsAdding(true);
+  };
+
+  const handleFormClose = () => {
+    setIsAdding(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!productName || !cost || !price || !stock || !category) {
+      alert('Please check you filled every section for sure');
+      return;
+    }
+
+    try {
+      const itemsRef = collection(database, 'users', userId, 'items');
+
+      const newDocRef = await addDoc(itemsRef, {
+        productName,
+        cost: Number(cost),
+        price: Number(price),
+        stock: Number(stock),
+        category,
+        createdAt: new Date(),
+      });
+
+      const newItem = {
+        id: newDocRef.id,
+        productName,
+        cost: Number(cost),
+        price: Number(price),
+        category,
+        createdAt: new Date(),
+      };
+
+      setItems((prevItems) => [...prevItems, newItem]);
+
+      // form init
+      setProductName('');
+      setCost(0);
+      setPrice(0);
+      setStock(0);
+      setCategory('');
+
+      alert('New item added!');
+    } catch (error) {
+      console.error('Falied to add item:', error);
+      alert('Failed to add item. Please check console.');
+    }
+  };
 
   useEffect(() => {
     // Check whether user log in or not
@@ -46,6 +106,74 @@ export default function Inventory() {
   return (
     <div>
       <h3>Inventory List</h3>
+      <button onClick={handleAddItem}>+ Add Item</button>
+
+      {/* Show add item section while isAdding status is true  */}
+      {/* 이 부분 css 할 때 팝업으로 만들기 */}
+      <div className='modal'>
+        {isAdding && (
+          <form onSubmit={handleSubmit} className='add-form'>
+            <label htmlFor='productName'>Product Name</label>
+            <input
+              id='productName'
+              type='text'
+              placeholder='Product Name'
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+              required
+            />
+
+            <label htmlFor='cost'>Cost</label>
+            <input
+              id='cost'
+              type='number'
+              value={cost}
+              onChange={(e) => setCost(Number(e.target.value))}
+              required
+              step='0.01'
+            />
+
+            <label htmlFor='price'>price</label>
+            <input
+              id='price'
+              type='number'
+              value={price}
+              onChange={(e) => setPrice(Number(e.target.value))}
+              required
+              step='0.01'
+            />
+
+            <label htmlFor='stock'>Count</label>
+            <input
+              id='stock'
+              type='number'
+              value={stock}
+              onChange={(e) => setStock(Number(e.target.value))}
+              required
+              step='0.01'
+            />
+
+            <label htmlFor='category'>Category</label>
+            <select
+              id='category'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
+              <option value=''>Choose category</option>
+              <option value='K-Pop Album'>K-Pop Album</option>
+              <option value='K-Pop Merch'>K-Pop Merch</option>
+              <option value='Stationary'>Stationary</option>
+              <option value='Plush'>Plush</option>
+              <option value='etc'>etc</option>
+            </select>
+
+            <button onClick={handleFormClose}>Cancel</button>
+            <button type='submit'>Complete</button>
+          </form>
+        )}
+      </div>
+
       {items.length > 0 ? (
         items.map((item) => (
           <div key={item.id}>
